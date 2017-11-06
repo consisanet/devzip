f82e1fc8aa37e5b44b1c07dfbf20ded278ca06ff
Diego Occhi <diegocchi@gmail.com>
2017-11-06 08:36:28 -0200

    compress

--- www/DevZip.js
+++ www/DevZip.js
@@ -3,3 +3,6 @@ var exec = require('cordova/exec');
 exports.decompressToString = function(zipFile, success, error) {
     exec(success, error, "DevZip", "decompressToString", [zipFile]);
 };
+exports.compress = function(str, success, error) {
+    exec(success, error, "DevZip", "compress", [str]);
+};
